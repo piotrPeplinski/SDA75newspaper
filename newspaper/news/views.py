afa49b74ea331a68685839b84a8d193aac8395a8
Piotr Pepliński <p.peplaj@gmail.com>
2023-10-22 15:13:57 +0200

    concrete view classes

--- newspaper/news/views.py
+++ newspaper/news/views.py
@@ -9,31 +9,12 @@ from rest_framework.views import APIView
 # Create your views here.
 
 
-class ListCreateArticles(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+class ListCreateArticles(generics.ListCreateAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class ArticleDetail(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
+class ArticleDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-    
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-    
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
-    
 
