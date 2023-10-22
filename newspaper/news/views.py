bd08d2fab53c1ae9370b702dca3f6baf053159fa
Piotr Pepliński <p.peplaj@gmail.com>
2023-10-22 11:03:55 +0200

    update

--- newspaper/news/views.py
+++ newspaper/news/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Article
+from .serializers import ArticleSerializer
 
 # Create your views here.
+
+
+def article_list(request):
+    articles = Article.objects.all()
+    serializer = ArticleSerializer(articles, many=True)
+    return JsonResponse({'articles': serializer.data})
