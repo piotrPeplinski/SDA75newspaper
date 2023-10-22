2658d60bd2e22ab41de89293a9bc0be6f5b8dce1
Piotr Pepliński <p.peplaj@gmail.com>
2023-10-22 11:58:19 +0200

    update

--- newspaper/news/urls.py
+++ newspaper/news/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('articles/', views.article_list, name='article_list'),
+    path('articles/', views.list_create_articles, name='articles'),
 ]
--- newspaper/news/views.py
+++ newspaper/news/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
-from django.http import JsonResponse
 from .models import Article
 from .serializers import ArticleSerializer
-
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
 # Create your views here.
 
-
-def article_list(request):
-    articles = Article.objects.all()
-    serializer = ArticleSerializer(articles, many=True)
-    return JsonResponse({'articles': serializer.data})
+@api_view(['GET','POST'])
+def list_create_articles(request):
+    if request.method == 'GET':
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+    else:
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
