766778078424bfd415e03f48de99ff41993c9633
Piotr Pepliński <p.peplaj@gmail.com>
2023-10-29 12:21:21 +0100

    update

--- newspaper/news/urls.py
+++ newspaper/news/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from . import views
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('articles/', views.ListCreateArticles.as_view(), name='articles'),
     path('users/', views.ListCreateUsers.as_view(), name='users'),
     path('articles/<int:pk>/',
          views.ArticleDetail.as_view(), name='article_detail'),
+    path('get-token/', obtain_auth_token),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
